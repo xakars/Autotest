96f5bd6324a89cb71eac18206a064e078cd7b716
Arsen Hakimov <xakars02@gmail.com>
2020-12-18 20:47:31 +0300

    Реализация LoginPage

---
+++ pages/main_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .login_page import LoginPage
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element_by_css_selector("#login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
