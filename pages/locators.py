1df52a74d9dbe8fd55f56721ee674961a40e88dc
Arsen Hakimov <xakars02@gmail.com>
2020-12-26 00:27:41 +0300

    update projeсt

--- pages/locators.py
+++ pages/locators.py
@@ -8,4 +8,18 @@ class LoginPageLocators():
     LOG_FORM = (By.CSS_SELECTOR, "#login_form")
     REG_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_BASK = (By.CSS_SELECTOR, "#add_to_basket_form")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_ABOUT_ADD = (By.CSS_SELECTOR, ".alert:nth-child(1) strong")
+    MESSAGE_ABOUT_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert:nth-child(3) strong")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR,".btn-group a")
+
+class BasketPageLocators():
+    BASKET_ITEM = (By.CSS_SELECTOR, "#basket_formset")
+    MESSAGE_ABOUT_CLEAR_BASKET = (By.CSS_SELECTOR, "#content_inner")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com"
 def test_guest_can_go_to_login_page(browser):
@@ -12,3 +13,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_page()
